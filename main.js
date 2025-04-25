2c6741ce0b09f6bc7ae59f8ed70e64c3d0c6d3c6
ndk <navnathkadam284@gmail.com>
2025-04-26 01:01:26 +0530

    Added Small Comment which is most important

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 
 // MongoDB Connection
 mongoose
+
+   // Replace 'mymongodb' with your MongoDB service name in Docker Compose
   .connect("mongodb://mymongodb:27017/mynodedb")
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
